95fd49d3346d2797b5b0f3ea8c00c2c3dd2bbaf2
Bruno Bertomeu <brunob@gamehouse.com>
2017-07-17 11:11:31 +0200

    lesson 16

---
+++ functions.py
@@ -0,0 +1,27 @@
+# print(1)
+#
+# def print_double(text):
+#     print(text * 2)
+#
+# print(2)
+#
+# print_double("Bruno")
+#
+# print(3)
+
+
+# def blah(text, text2):
+#     a = str(1) + text + text2
+#     return a
+#
+# print(blah("Bruno", "Elaine"))
+
+# def blah(text, text2):
+#     a = str(1) + text + text2
+#
+# print(blah("Bruno", "Elaine"))
+
+def add(num1, num2, num3=3, num4=4):
+    return num1 + num2 + num3 + num4
+
+print(add(5, 7, num4=10))
--- undutchify_simple.py
+++ undutchify_simple.py
@@ -1,4 +1,9 @@
 name = input("Please enter you Dutch name: ")
 vowels = ['a', 'e', 'u', 'i', 'o']
 
-name.
+name = name.replace("ij", "y")
+
+for vowel in vowels:
+    name = name.replace(vowel * 2, vowel)
+
+print(name)

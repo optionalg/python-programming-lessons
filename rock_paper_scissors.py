5b9aa9bbed8e5595a4a3c3107cdf63264a268e1c
Bruno Bertomeu <brunob@gamehouse.com>
2017-08-15 15:29:04 +0200

    added compare function (WIP)

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,7 +7,7 @@
 # ask the players their name
 # ask their choice
 # check their choice (needs to be valid)
-# find a way to hide the choice???? (print many lines)
+# find a way to hide the choice???? https://stackoverflow.com/questions/2084508/clear-terminal-in-python
 # compare the choices
 # announce the winner
 
@@ -17,6 +17,11 @@
 
 # high scores (in a file)
 # extend the game: add more choices (lizard, spock...)
+
+import os
+
+# order is important, the next element beasts the current one
+choices = ["rock", "paper", "scissors"]
 
 player1 = input('Name of player 1: ')
 player2 = input('Name of player 2: ')
@@ -34,10 +39,34 @@ def check(question, solutions):
             print("Please try again!")
 
 
-choice1 = check(player1 + ": enter your choice (rock, paper or scissors)",
-                ["rock", "paper", "scissors"])
-choice2 = check(player2 + ": enter your choice (rock, paper or scissors)",
-                ["rock", "paper", "scissors"])
+
+def compare(first, second):
+    """
+    returns 2 if second beats first, 1 if first beats seconds, 0 if they are equal
+    """
+    if first not in choices or second not in choices:
+        print("Error: invalid arguments for compare")
+        return None
+
+    index1 = choices.index(first)
+    index2 = choices.index(second)
+
+    # check if first equals second, if so return 0
+    # check if second beats first
+    if index2 == index1 + 1:
+        return 2
+    # otherwise, first beasts second, return 1
+
+
+choice1 = check(player1 + ": enter your choice (rock, paper or scissors) ",
+                choices)
+
+os.system("cls||clear")
+
+choice2 = check(player2 + ": enter your choice (rock, paper or scissors) ",
+                choices)
 
 print(player1 + " choice is " + choice1)
 print(player2 + " choice is " + choice2)
+
+print(compare(choice1, choice2))
